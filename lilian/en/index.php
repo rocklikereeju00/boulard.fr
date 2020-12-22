0b66ab33b94cbe1c1f3132e6a6e873cb434770b3
Lilian <33222654+Phaide@users.noreply.github.com>
2020-12-22 22:00:28 +0100

    Add files via upload

---
+++ lilian/en/index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Lilian Boulard</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../common/assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="../common/assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome!</a></li>
+							<li><a href="#one">My subjects/projects</a></li>
+							<li><a href="#two">Who am I?</a></li>
+							<li><a href="#three">Get in touch</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Lilian Boulard</h1>
+							<h2>Artificial Intelligence engineer</h2>
+							<br />
+							<ul class="actions">
+								<li><a href="https://lilian.boulard.fr/en/portfolio/" class="button scrolly">Portfolio</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="https://github.com/sami-dca/sami_dca" class="image"><img src="../common/images/sami_logo.png" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Sami, the decentralized communication application</h2>
+									<p>
+										Originally a personnal project, Sami became a university project as part of my Bachelor's Degree. <br />
+										This software allows for textual messaging on a decentralised network,
+										much like <a target="_blank" href="https://jami.net/">Jami</a>.
+									</p>
+									<ul class="actions">
+										<li><a href="https://github.com/sami-dca/sami_dca" class="button">Discover</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="https://github.com/dirty-cat/dirty_cat" class="image"><img src="../common/images/dirty_cat.svg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>dirty_cat</h2>
+									<p>
+										dirty_cat is a Python library for statistical learning on non-curated categorical variables.<br />
+										This project was initiated <a target="_blank" href="https://www.linkedin.com/in/patricio-cerda/">Patricio Cerda</a> as part of his PhD thesis.<br />
+										<a target="_blank" href="http://gael-varoquaux.info">Gaël Varoquaux</a>, Research Director, is currently supervising my work.
+									</p>
+									<ul class="actions">
+										<li><a href="https://github.com/dirty-cat/dirty_cat" class="button">Discover</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="https://github.com/Phaide/PKS" class="image"><img src="../common/images/door-1106012__340.webp" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Knock knock</h2>
+									<p>
+										PKS is a personnal project aiming to secure a server's access using a dynamic sequence port-knocking system.
+									</p>
+									<ul class="actions">
+										<li><a href="https://github.com/Phaide/PKS" class="button">Discover</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Who am I?</h2>
+							<p>
+								I, [REDACTED], am a French student in Computer Science,
+								specialised in software engineering applied to Artificial Intelligence.<br />
+								Apprentice in a french research institute during the day, developer of
+								<a title="HAL 9000... Stanley Kubrick ! No ? Really !? Oh, that's a shame...">HAL 9000</a> during the week-end.<br />
+							</p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-graduation-cap"></span>
+									<h3>Studies</h3>
+									<p>
+										I am currently doing my Bachelor's Degree in software engineering.<br />
+										Once done, I will be attending a specialized Master in Artificial Intelligence<br />
+										From here on out, I get to choose between:
+										<ul>
+											<li>Being transfered to the <a target="_blank" href="https://en.wikipedia.org/wiki/University_of_California,_Los_Angeles">UCLA</a> for my second (and last) year</li>
+											<li>Finish my Master in France, and continue in an engineering school.</li>
+										</ul>
+										Time will tell !
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-microscope"></span>
+									<h3>Work</h3>
+									<p>
+										I currently work as a Machine Learning and Software Engineering apprentice at the
+										<a target="_blank" href="https://en.wikipedia.org/wiki/French_Institute_for_Research_in_Computer_Science_and_Automation">French National Institute for Reasearch in Computer Science and Automation</a> (<i>Inria</i>),
+										as part of the <a target="_blank" href="https://team.inria.fr/parietal/">Parietal team</a>.<br />
+										It is a 3-year contract (which is rather scarce in public research institutes!), which focuses on machine learning libraries development in Python.<br />
+										As of November 2020, I work on dirty_cat, and will come to work somewhat soon on <a target="_blank" href="https://en.wikipedia.org/wiki/Scikit-learn">Scikit-Learn</a>,
+										an extremely popular machine learning library, developed mainly by <i>Inria</i> !
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>Programming</h3>
+									<p>
+										I work as a software developer, so I somewhat like writting code.<br />
+										<br />
+										I am a fellow member of the Python sect.
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>Cryptography</h3>
+									<p>
+										Anonymity and decentralization on Internet are two of my great interests !<br />
+										<br />
+										<i>
+										<i>Quite ironically, we can note that I give my full name on the web.</i><br />
+										<br />
+										If I didn't have the opportunity to work at <a target="_blank" href="https://team.inria.fr/parietal/">Inria Parietal</a>,
+										I would've probably applied for <a target="_blank" href="https://team.inria.fr/wide/">Inria WIDE</a> :p <br />
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-seedling"></span>
+									<h3>Nature is epic</h3>
+									<p>
+										With this icon, did you expect me to show you how much <i>eco-friendly</i> I am ? <br />
+										'cause that's what I'm 'bout to do ! <br />
+										Mid-2019, I got very interested in botany, and by that I don't especially mean gardening,
+										but rather organisms evolution, which
+										Courant 2019, je me suis pris de fascination pour la botanique,
+										et par là je ne parle pas de jardinage, mais de l'évolution des organismes,
+										which amazes me ! <br />
+										I am currently the very owner of a vivarium ! <br />
+										If I wasn't in computer science, I'd probably would've become biologist.
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-music"></span>
+									<h3></h3>
+									<p>
+										Well I did until I listened to some music ! <br />
+										Did you ever heard music ? Because that's some next level <i>epicness</i> ! <br />
+										So much so that I learned to do it my-very-self: I am a self-taught pianist since 14, and guitarist since 17 !<br />
+										<br />
+									</p>
+								</section>
+							</div>
+							<ul class="actions">
+								<li><a href="https://lilian.boulard.fr/fr/portfolio/moi" class="button">Portfolio, section "Moi"</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Get in touch</h2>
+							<p>
+								I am available at the following contact information for any contact, pro or not ! <br />
+								Please be polite ; advertisement is prohibited !
+							</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Name / Alias</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Email address</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Message</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Send</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Email</h3>
+											<a title="Spelled to avoid spam !">lilian at boulard dot fr</a>
+										</li>
+										<li>
+											<h3>Réseaux</h3>
+											<ul class="icons">
+												<li><a href="https://github.com/Phaide" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://www.instagram.com/phaide_phd/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="https://www.linkedin.com/in/lilian-boulard/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; <?php echo date("Y"); // Prints the current year ?> Lilian Boulard. All rights reserved.</li>
+						<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="../common/assets/js/jquery.min.js"></script>
+			<script src="../common/assets/js/jquery.scrollex.min.js"></script>
+			<script src="../common/assets/js/jquery.scrolly.min.js"></script>
+			<script src="../common/assets/js/browser.min.js"></script>
+			<script src="../common/assets/js/breakpoints.min.js"></script>
+			<script src="../common/assets/js/util.js"></script>
+			<script src="../common/assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ lilian/fr/index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Lilian Boulard</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../common/assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="../common/assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Bienvenue!</a></li>
+							<li><a href="#one">Mes sujets/projets</a></li>
+							<li><a href="#two">Qui suis-je ?</a></li>
+							<li><a href="#three">Me contacter</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Lilian Boulard</h1>
+							<h2>Ingénieur en Intelligence Artificielle</h2>
+							<br />
+							<ul class="actions">
+								<li><a href="https://lilian.boulard.fr/fr/portfolio/" class="button scrolly">Portfolio</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="https://github.com/sami-dca/sami_dca" class="image"><img src="../common/images/sami_logo.png" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Sami, l'application de communication décentralisée</h2>
+									<p>
+										Originellement projet personnel, Sami est passé projet universitaire dans le cadre de mon bachelor. <br />
+										C'est un logiciel permettant de s'échanger des messages sur un réseau décentralisé,
+										à l'instar de <a target="_blank" href="https://jami.net/">Jami</a>.
+									</p>
+									<ul class="actions">
+										<li><a href="https://github.com/sami-dca/sami_dca" class="button">Découvrir</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="https://github.com/dirty-cat/dirty_cat" class="image"><img src="../common/images/dirty_cat.svg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>dirty_cat</h2>
+									<p>
+										dirty_cat est une librairie Python permettant l'apprentissage statistique sur des catégories désorganisées.<br />
+										C'est un projet initié par <a target="_blank" href="https://www.linkedin.com/in/patricio-cerda/">Patricio Cerda</a> dans le cadre de son doctorat.<br />
+										<a target="_blank" href="http://gael-varoquaux.info">Gaël Varoquaux</a>, Directeur de Recherche, dirige actuellement mes travaux.
+									</p>
+									<ul class="actions">
+										<li><a href="https://github.com/dirty-cat/dirty_cat" class="button">Découvrir</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="https://github.com/Phaide/PKS" class="image"><img src="../common/images/door-1106012__340.webp" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Toc toc</h2>
+									<p>
+										PKS est un projet personnel visant à sécuriser l'accès d'un serveur en utilisant une technologie de port-knocking à séquence tournante.
+									</p>
+									<ul class="actions">
+										<li><a href="https://github.com/Phaide/PKS" class="button">Découvrir</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Qui suis-je ?</h2>
+							<p>
+								De mon vrai nom [RÉDIGÉ], je suis étudiant en informatique,
+								spécialisé en ingénierie logicielle appliquée à l'Intelligence Artificielle.<br />
+								Apprenti dans un institut de recherche le jour,
+								développeur de <a title="HAL 9000... Stanley Kubrick ! Non, vraiment, ça ne vous dis toujours rien !? Oh, quand même...">HAL 9000</a> le week-end.<br />
+							</p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-graduation-cap"></span>
+									<h3>Études</h3>
+									<p>
+										Je suis actuellement en troisième année de Bachelor Informatique, option ingénierie logicielle.<br />
+										Une fois mon bachelor en poche, je me dirige vers un mastère spécialisé en Intelligence Artificielle.<br />
+										De là j'ai deux options:
+										<ul>
+											<li>Faire un transfert avec l'<a target="_blank" href="https://fr.wikipedia.org/wiki/Universit%C3%A9_de_Californie_%C3%A0_Los_Angeles">UCLA</a> pour ma deuxième année de mastère</li>
+											<li>Finir mon mastère en France, et continuer sur une école d'ingénieur</li>
+										</ul>
+										Le temps nous le dira !
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-microscope"></span>
+									<h3>Travail</h3>
+									<p>
+										Je travaille actuellement comme "Apprenti en Machine Learning et Génie Logiciel" à
+										l'<a target="_blank" href="https://fr.wikipedia.org/wiki/Institut_national_de_recherche_en_informatique_et_en_automatique">Institut National de Recherche en Informatique et en Automatique</a> (l'<i>Inria</i>),
+										dans l'équipe-projet <a target="_blank" href="https://team.inria.fr/parietal/">Parietal</a>.<br />
+										C'est un contrat de 3 ans (plutôt rare en institut de recherche!), qui se focalise sur le développement de librairies de machine learning en Python.<br />
+										Actuellement (novembre 2020), je travaille sur dirty_cat,
+										et serai ammené dans un futur relatif à travailler sur <a target="_blank" href="https://en.wikipedia.org/wiki/Scikit-learn">Scikit-Learn</a>,
+										une libraire extrêmement populaire, développée en grande partie par l'<i>Inria</i>.
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>Programmation</h3>
+									<p>
+										Développeur applicatif de profession, j'aime bien écrire du code.<br />
+										<br />
+										Je suis un membre aguerri de la secte Python.
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>Cryptographie</h3>
+									<p>
+										L'anonymat et la décentralisation sur Internet sont mes grandes passions !<br />
+										<br />
+										<i>De manière assez ironique, on peut noter que je donne mon nom en clair sur le web.</i> <br />
+										<br />
+										Si je n'avais eu l'opportunité de travailler à <a target="_blank" href="https://team.inria.fr/parietal/">Inria Parietal</a>,
+										j'aurai sans doute postulé à <a target="_blank" href="https://team.inria.fr/wide/">Inria WIDE</a> :p<br />
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-seedling"></span>
+									<h3>La nature c'est stylé</h3>
+									<p>
+										Avez cette icône, vous attendiez-vous à ce que je vous démontre à quel point je suis <i>eco-friendly</i> ? <br />
+										Et bien c'est ce que je vais faire !<br />
+										Courant 2019, je me suis pris de fascination pour la botanique,
+										et par là je ne parle pas de jardinage, mais de l'évolution des organismes,
+										ce qui m'émerveille ! <br />
+										Je suis aujourd'hui l'heureux propriétaire d'un vivarium ! <br />
+										Si je ne m'étais pas spécialisé dans l'informatique, je serai probablement devenu biologiste.
+									</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-music"></span>
+									<h3></h3>
+									<p>
+										Enfin je voulais devenir biologiste jusqu'à ce que j'écoute de la musique ! <br />
+										Avez-vous déjà écouté de la musique ? Parce que ça aussi c'est <i><a title="Stylé, dans la langue de Kanye West">hype</a></i>.<br />
+										Tellement <i><a title="Hype, francisé. Ethymologiquement, ça n'a aucun sens">hypé</a></i> que j'ai appris à en faire moi-même: je suis pianiste autodidacte depuis l'âge de 14 ans, et guitariste depuis mes 17 !<br />
+										<br />
+									</p>
+								</section>
+							</div>
+							<ul class="actions">
+								<li><a href="https://lilian.boulard.fr/fr/portfolio/moi" class="button">Portfolio, section "Moi"</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Me contacter</h2>
+							<p>
+								Je suis disponible aux coordonnées suivantes pour tout contact, professionnel ou non ! <br />
+								Merci de rester courtois ; toute publicité est totalement proscrite !
+							</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Nom / Pseudo</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Adresse email</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Message</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Envoyer</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Email</h3>
+											<a title="Volontairement épelé pour éviter le spam!">lilian arobase boulard point fr</a>
+										</li>
+										<li>
+											<h3>Réseaux</h3>
+											<ul class="icons">
+												<li><a href="https://github.com/Phaide" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://www.instagram.com/phaide_phd/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="https://www.linkedin.com/in/lilian-boulard/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; <?php echo date("Y"); // Prints the current year ?> Lilian Boulard. Tous droits réservés.</li>
+						<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="../common/assets/js/jquery.min.js"></script>
+			<script src="../common/assets/js/jquery.scrollex.min.js"></script>
+			<script src="../common/assets/js/jquery.scrolly.min.js"></script>
+			<script src="../common/assets/js/browser.min.js"></script>
+			<script src="../common/assets/js/breakpoints.min.js"></script>
+			<script src="../common/assets/js/util.js"></script>
+			<script src="../common/assets/js/main.js"></script>
+
+	</body>
+</html>
