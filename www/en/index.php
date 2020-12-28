d7b8c684287158f7e9d7ca178fbdd8b3ccaee01b
Lilian Boulard <lilian@boulard.fr>
2020-12-28 19:50:47 +0100

    Add www pages and fix translations

---
+++ www/en/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+    <head>
+		<title>Boulard family's website</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../common/assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="../common/assets/css/noscript.css" /></noscript>
+	</head>
+
+	<body class="is-preload">
+		<!-- Wrapper -->
+        <div id="wrapper">
+
+            <!-- Header -->
+            <header id="header">
+                <div class="inner">
+
+                    <!-- Nav -->
+                    <!--
+                    <nav>
+                        <ul>
+                            <li><a href="#menu">Menu</a></li>
+                        </ul>
+                    </nav>
+                    -->
+
+                </div>
+            </header>
+
+            <!-- Menu -->
+            <!--
+            <nav id="menu">
+                <h2>Menu</h2>
+                <ul>
+                    <li><a href="index">Home</a></li>
+                </ul>
+            </nav>
+            -->
+
+            <!-- Main -->
+            <div id="main">
+                <div class="inner">
+                    <header>
+                        <h1>Welcome to boulard.fr !</h1>
+                        <p>
+                            This page is a hub for the individual pages of the Boulard family's members.<br />
+                        </p>
+                        <hr />
+                        <p>
+                            <i>A quick lesson, if you will</i><br />
+                            The last name "Boulard" seems to have a german etymology, derived from "Bolohard"
+                            (<i>bolo</i> being friend/brother, <i>hard</i> being hard/strong),
+                            according to <a target="_blank" href="https://www.geneanet.org/nom-de-famille/BOULARD">Geneanet</a>.
+                            It is mainly spread accross metropolitan France.<br />
+                            According to the <a target="_blank" href="https://www.insee.fr/fr/statistiques/3536630">INSEE</a>
+                            (The French Institute for Statistics and Economic Studies),
+                            there have been 7239 births with the name "Boulard" between 1891 et 2000.
+                        </p>
+                    </header>
+                    <hr />
+                    <h2>Pages</h2>
+                    <section class="tiles">
+                        <article class="style1">
+                            <span class="image">
+                                <?php
+                                $email = "lilian@boulard.fr";
+                                $hash = md5(strtolower($email));
+                                ?>
+                                <img src="https://gravatar.com/avatar/<?php echo $hash ?>" alt="Lilian's profile picture" />
+                            </span>
+                            <a href="https://lilian.boulard.fr/en">
+                                <h2>Lilian</h2>
+                                <div class="content">
+                                    <p>
+                                        Lilian works as a software engineer at the French Institute for Research in Computer Science and Automation.<br />
+                                        He specializes in Artificial Intelligence.
+                                    </p>
+                                </div>
+                            </a>
+                        </article>
+                        <!--
+                        <article class="style2">
+                            <span class="image">
+                                <img src="../common/images/pic02.jpg" alt="" />
+                            </span>
+                            <a href="generic.html">
+                                <h2>Lorem</h2>
+                                <div class="content">
+                                    <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+                                </div>
+                            </a>
+                        </article>
+                        <article class="style3">
+                            <span class="image">
+                                <img src="../common/images/pic03.jpg" alt="" />
+                            </span>
+                            <a href="generic.html">
+                                <h2>Feugiat</h2>
+                                <div class="content">
+                                    <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+                                </div>
+                            </a>
+                        </article>
+                        -->
+                    </section>
+                    <hr />
+                    <section>
+                        <h3>Website's source</h3>
+                        <p>
+                            <span class="image left"><a target="_blank" href="https://github.com/Phaide/website"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Github logo" width="160" height="160" /></a></span>
+                            As we are very modern people, we share the open-source mindset (at least the website's administrator does!).<br />
+                            What does this mean ?<br />
+                            Well, if you wish to reuse the source code of this website, or request modifications, or anything really,
+                            you can do so by having a look at the <a target="_blank" href="https://github.com/Phaide/website">GitHub's repository</a> !
+                        </p>
+                    </section>
+                    <hr />
+                    <section>
+                        <h3>My name is also Boulard !</h3>
+                        <p>
+                            <!--<span class="image right"><img src="" alt="" /></span>-->
+                            What ?<br />
+                            You mean we're not the only ones ?<br />
+                            Well, because on top of being modern, we're nice,
+                            we can offer you the possibility to host your very own page and email address on this domain !<br />
+                            If you are interested, please use the <a href="#footer">contact form below</a> to get in touch with the administrator.<br />
+                            <br />
+                            PS: The domain is not for sale (and probably never will).
+                        </p>
+                    </section>
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <footer id="footer">
+                <div class="inner">
+                    <section>
+                        <h2>Get in touch</h2>
+                        <p>
+                            Beware: this form doesn't work yet, please come back in a few days !
+                        </p>
+                        <form method="post" action="#">
+                            <div class="fields">
+                                <div class="field half">
+                                    <input type="text" name="name" id="name" placeholder="Name" />
+                                </div>
+                                <div class="field half">
+                                    <input type="email" name="email" id="email" placeholder="Email" />
+                                </div>
+                                <div class="field">
+                                    <textarea name="message" id="message" placeholder="Message"></textarea>
+                                </div>
+                            </div>
+                            <ul class="actions">
+                                <li><input type="submit" value="Send" class="primary" /></li>
+                            </ul>
+                        </form>
+                    </section>
+                    <section>
+                        <h2>Contact</h2>
+                        <p>
+                            To contact the website's administrator, please use the side form.<br />
+                            If you wish to contact a specific person, please head over to their page (all available pages are listed above).
+                        </p>
+                    </section>
+                    <ul class="copyright">
+                        <li>&copy; <?php echo date("Y"); // Prints the current year ?> Boulard.fr. All rights reserved.</li>
+                        <li>Design by <a target="_blank" href="https://html5up.net/">HTML5UP</a></li>
+                    </ul>
+                </div>
+            </footer>
+
+        </div>
+
+		<!-- Scripts -->
+        <script src="../common/assets/js/jquery.min.js"></script>
+        <script src="../common/assets/js/browser.min.js"></script>
+        <script src="../common/assets/js/breakpoints.min.js"></script>
+        <script src="../common/assets/js/util.js"></script>
+        <script src="../common/assets/js/main.js"></script>
+
+	</body>
+</html>
