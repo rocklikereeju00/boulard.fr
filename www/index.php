f72061af712407002f995b4007d409d17ff3ca90
Lilian Boulard <lilian@boulard.fr>
2020-12-28 19:50:20 +0100

    Add www pages and fix translations

---
+++ www/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+    <head>
+        <title>Boulard.fr</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="common/assets/css/main.css" />
+        <noscript><link rel="stylesheet" href="common/assets/css/noscript.css" /></noscript>
+    </head>
+    <body class="is-preload">
+
+        <!-- Wrapper -->
+        <div id="wrapper">
+
+            <!-- Main -->
+            <section id="main" class="wrapper">
+                <div class="inner">
+
+                    <h1 class="major">
+                        Hello, and welcome !<br />
+                        Please choose a language to continue !
+                    </h1>
+
+                    <hr />
+
+                    <section>
+                        <div class="box alt">
+                            <div class="row gtr-uniform">
+                                <div class="col-6"><span class="image fit"><a href="fr/index"><img src="http://common.boulard.fr/assets/flags/4x3/fr.svg" alt="Français" /></a></span></div>
+                                <div class="col-6"><span class="image fit"><a href="en/index"><img src="http://common.boulard.fr/assets/flags/4x3/us.svg" alt="English" /></a></span></div>
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+            </section>
+
+        </div>
+
+        <!-- Scripts -->
+        <script src="common/assets/js/jquery.min.js"></script>
+        <script src="common/assets/js/browser.min.js"></script>
+        <script src="common/assets/js/breakpoints.min.js"></script>
+        <script src="common/assets/js/util.js"></script>
+        <script src="common/assets/js/main.js"></script>
+
+    </body>
+</html>
